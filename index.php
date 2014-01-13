e4410f16160728c3ae91a6c967b72482799d8a00
Mark <mark@digitalbrandmine.com>
2014-01-13 07:59:50 -0500

    jquery

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <html>
-<body>
-<h1>First Heroku App - Changed</h1>
-<?php
-echo "Hello World!";
-?>
-</body>
+    <head> 
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    </head> 
+    <body> 
+        <form action=""> 
+            <input type="button" value="button" onclick="getData()" /> 
+        </form> 
+        <div id="myelement"> Some text... </div> 
+        <?php
+        echo "Hello World!";
+        ?>
+        <p>
+            <button id="buttonpressme">Press Me</button>
+        </p>
+        <script>
+                $("buttonpressme").click(function() {
+                    alert("Pressed!");
+                });
+        </script>
+    </body>
 </html>
