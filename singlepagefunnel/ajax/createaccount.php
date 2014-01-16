62b40c48946a27c58b3d831977aeac96b6a7d1b7
Mark <mark@digitalbrandmine.com>
2014-01-15 23:34:07 -0500

    starting to work ...

---
+++ singlepagefunnel/ajax/createaccount.php
@@ -0,0 +1,50 @@
+<?php
+
+$success = false;
+$verificationcode = "";
+$failureMsg = "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // error_log('$_POST = ' . print_r($_POST, true));
+    if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+
+        $conn = new mysqli("localhost", "gaexamples", "daint4toot", "gaexamples", 8889);
+        if ($conn->connect_errno) {
+            error_log(sprintf("Connect failed (%s): %s\n", $conn->connect_errno, $conn->connect_error));
+        } else {
+            // check if username already exists
+            $sqlUsername = "SELECT * FROM singlepagefunnel WHERE username = ?";
+            $stmt = $conn->prepare($sqlUsername);
+            $stmt->bind_param('s', $_POST['username']);
+            $stmt->execute();
+            $stmt->store_result();
+            if ($stmt->num_rows > 0) {
+                $failureMsg = "Username already exists: " . $_POST['username'];
+            } else {
+                // check if email already exists
+                $sqlEmail = "SELECT * FROM singlepagefunnel WHERE email = ?";
+                $stmt = $conn->prepare($sqlEmail);
+                $stmt->bind_param('s', $_POST['email']);
+                $stmt->execute();
+                $stmt->store_result();
+                if ($stmt->num_rows > 0) {
+                    $failureMsg = "Email already exists: " . $_POST['email'];
+                } else {
+                    // insert new account
+                    $sql = "INSERT INTO singlepagefunnel VALUES (?, ?, ?, ?)";
+                    $stmt = $conn->prepare($sql);
+                    // generate random 5 character verification code
+                    $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+                    $verificationcode = '';
+                    for ($i = 0; $i < 5; $i++) {
+                        $verificationcode .= $characters[rand(0, strlen($characters) - 1)];
+                    }
+                    $stmt->bind_param('ssss', $_POST['username'], $_POST['email'], $_POST['password'], $verificationcode);
+                    $success = $stmt->execute();
+                }
+            }
+            mysqli_close($conn);
+        }
+    }
+}
+echo json_encode(array('success' => $success, 'verificationcode' => $verificationcode, 'failureMsg' => $failureMsg));
+?>
--- singlepagefunnel/index.php
+++ singlepagefunnel/index.php
@@ -40,12 +40,12 @@
     <body>
 
         <!-- create account form -->
-        <div id="dialog-form" class="signup-modal" title="Create new user">
+        <div id="create-account-dialog" class="signup-modal" title="Create new user">
             <p class="validateTips">All form fields are required.</p>
             <form>
                 <fieldset>
-                    <label for="name">Username</label>
-                    <input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" class="text ui-widget-content ui-corner-all">
                     <label for="email">Email</label>
                     <input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all">
                     <label for="password">Password</label>
@@ -109,10 +109,10 @@
                         }
                 );
 
-                var name = $("#name"),
+                var username = $("#username"),
                         email = $("#email"),
                         password = $("#password"),
-                        allFields = $([]).add(name).add(email).add(password),
+                        allFields = $([]).add(username).add(email).add(password),
                         tips = $(".validateTips");
 
                 function updateTips(t) {
@@ -145,7 +145,7 @@
                     }
                 }
 
-                $("#dialog-form").dialog({
+                $("#create-account-dialog").dialog({
                     autoOpen: false,
                     height: 350,
                     width: 300,
@@ -156,22 +156,34 @@
                             var bValid = true;
                             allFields.removeClass("ui-state-error");
 
-                            bValid = bValid && checkLength(name, "username", 3, 16);
+                            bValid = bValid && checkLength(username, "username", 3, 16);
                             bValid = bValid && checkLength(email, "email", 6, 80);
                             bValid = bValid && checkLength(password, "password", 5, 16);
 
-                            bValid = bValid && checkRegexp(name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter.");
+                            bValid = bValid && checkRegexp(username, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter.");
                             // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
                             bValid = bValid && checkRegexp(email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com");
                             bValid = bValid && checkRegexp(password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9");
 
-                            if (bValid) {
-                                $("#users tbody").append("<tr>" +
-                                        "<td>" + name.val() + "</td>" +
-                                        "<td>" + email.val() + "</td>" +
-                                        "<td>" + password.val() + "</td>" +
-                                        "</tr>");
-                                alert("creating the account ...");
+                            if (bValid) {                                
+                                $.ajax({
+                                    url: 'ajax/createaccount.php',
+                                    type: "POST",
+                                    data: {
+                                        username: username.val(),
+                                        email: email.val(),
+                                        password: password.val()
+                                    },
+                                    dataType: "JSON"
+                                }).done(function(resp) {
+                                    console.log("resp = " + resp);
+                                    console.log("respObj.success = " + resp.success);
+                                    console.log("respObj.verificationcode = " + resp.verificationcode);
+                                    console.log("respObj.failureMsg = " + resp.failureMsg);
+                                })
+                                        .fail(function() {
+                                    console.log("POST failed!");
+                                });
                                 $(this).dialog("close");
                             }
                         },
@@ -186,7 +198,7 @@
 
                 $("#create-account-menu-item")
                         .click(function() {
-                    $("#dialog-form").dialog("open");
+                    $("#create-account-dialog").dialog("open");
                 });
             });
         </script>
