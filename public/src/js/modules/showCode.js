4f8b8a427708137ba1ba0429080a059336e2a23a
Yaron <yaron@digitalbrandmine.com>
2014-03-06 09:27:18 -0500

    showCode enhancement experiment

--- public/src/js/modules/showCode.js
+++ public/src/js/modules/showCode.js
@@ -26,7 +26,7 @@
 		}
 		
 		codeWindow.innerHTML = codeSegment;
-		dbm.removeClass(codeWindow, 'prettyprinted')
+		dbm.removeClass(codeWindow, 'prettyprinted');
 		prettyPrint();
 
 	//Scroll the highlighted row to the center of the code window.	
@@ -40,6 +40,56 @@
 	dbm.showCode = showCode;
 	
 })(window.Dbm, window.jQuery);
+
+
+
+//(function(dbm, $) {
+//	var EOL = "\n",
+//		code = document.getElementById('this-file').innerHTML,
+//		doc = window.parent.document,
+////		len = codeArr.length,
+//		prettyPrint = window.prettyPrint || (window.top && window.top.prettyPrint) || function() {},
+//		highlightReg = /<div id\=\"_scTopLine\" class\=\"highlighter\">(.+)<\/div>/g;
+//		
+//	code = code.replace(/\bdbm\.showCode\([^\n]+\n+/g, '');	
+//	
+//	function showCode(page) {
+//		var codeSegment = "",
+//			i = -1,
+//			codeWindow = doc.getElementById("code-window"),
+//			highlightRow,
+//			codeWindowHeight,
+//			topLineDivHeight,
+//			codeTop,
+//			exp = new RegExp("(_gaq\\.push\\(.+" + page + "\"\\]\\);)");
+//		
+//		codeSegment = code.replace(exp, "<div id=\"_scTopLine\" class=\"highlighter\">$1</div>" );
+//		// - Remove lines with calls to dbm.showCode.
+//		// - Highlight the line with the call to _gaq.push
+////		while (++i < len) {
+////			if (codeArr[i].indexOf("dbm.showCode(") !== -1) { continue; }
+////			if (codeArr[i].indexOf("_gaq.push") !== -1 && codeArr[i].indexOf(page) !== -1) {
+////				codeSegment += "<div id=\"_scTopLine\" class=\"highlighter\">" + codeArr[i] + "</div>";
+////			} else {
+////				codeSegment += codeArr[i] + EOL;
+////			}	
+////		}
+//		
+//		codeWindow.innerHTML = codeSegment;
+//		dbm.removeClass(codeWindow, 'prettyprinted')
+//		prettyPrint();
+//
+//	//Scroll the highlighted row to the center of the code window.	
+//		highlightRow = doc.getElementById("_scTopLine");
+//		codeWindowHeight = codeWindow.offsetHeight;
+//		topLineDivHeight = highlightRow.offsetHeight;
+//		codeTop = highlightRow.offsetTop - ((codeWindowHeight - topLineDivHeight) / 2);
+//		$(codeWindow).animate({scrollTop: codeTop}, 500);
+//	}
+//	
+//	dbm.showCode = showCode;
+//	
+//})(window.Dbm, window.jQuery);
 
 
 
